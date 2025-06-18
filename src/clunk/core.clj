0a923173703d572084f45ea4478532f4f017cfce
Kimbsy <lordkimber@gmail.com>
2025-06-18 21:37:49 +0100

    use `iteration` to make the main loop functional
    also meant we can remove the `state` atom and all the `swap!`s and
    `reset!`s
    
    need to figure out a nice way of handling keyboard/mouse input now
    though :/

--- src/clunk/core.clj
+++ src/clunk/core.clj
@@ -26,7 +26,8 @@
             [clunk.util :as u]
             [clunk.palette :as p]
             [clunk.collision :as collision]
-            [clunk.tween :as tween]))
+            [clunk.tween :as tween]
+            [clunk.core :as c]))
 
 ;; FEATURES
 
@@ -41,13 +42,12 @@
 
 (def audio? false)
 
+;; @TODO pare this down
 (def initial-state
   {:window nil
    :scenes {:demo {:sprites []}}
    :current-scene :demo
    :debug? false})
-
-(def state (atom initial-state))
 
 ;; capture and restore the blending state of GL so we can draw NanoVG
 ;; text (which blats the config) and then get it back for drawing
@@ -67,7 +67,7 @@
   (GL30/glBlendFuncSeparate src-rgb dst-rgb src-alpha dst-alpha))
 
 (defn cleanup-audio
-  [{{:keys [source al-buffer context device]} :audio}]
+  [{:keys [source al-buffer context device]}]
   (AL10/alSourceStop source)
   (AL10/alDeleteSources source)
   (AL10/alDeleteBuffers al-buffer)
@@ -86,11 +86,11 @@
   (GL11/glLoadIdentity))
 
 (defn start-event-polling
-  []
+  [state]
   ;; putting event polling in a separate thread (didn't do much for
   ;; mouse event flood though)
   (future
-    (while (not (GLFW/glfwWindowShouldClose (:window @state)))
+    (while (not (GLFW/glfwWindowShouldClose (:window state)))
       ;; poll for window events, the key callback above will only be
       ;; invoked during this call
       (GLFW/glfwPollEvents)
@@ -135,133 +135,136 @@
              :context context
              :device device}))))))
 
+;; @TODO: split this up into functions and do a (-> state ....) thread
 (defn init
   [{[initial-window-width initial-window-height] :size
-    :as game}]
-  ;; @TODO: still needed?
-  ;; reset the game state
-  (reset! state initial-state)
+    :as game-config}]
+  (let [state game-config]
+    ;; set up an error callback, the default implementation will print
+    ;; the error message in System.err
+    (-> (GLFWErrorCallback/createPrint System/err)
+        .set)
 
-  ;; set up an error callback, the default implementation will print
-  ;; the error message in System.err
-  (-> (GLFWErrorCallback/createPrint System/err)
-      .set)
+    ;; initialise GLFW, most GLFW functions will not work before doing this
+    (when (not (GLFW/glfwInit))
+      (throw (IllegalStateException. "Unable to initialise GLFW")))
 
-  ;; initialise GLFW, most GLFW functions will not work before doing this
-  (when (not (GLFW/glfwInit))
-    (throw (IllegalStateException. "Unable to initialise GLFW")))
+    ;;;; configure GLFW  
+    ;; optional, the current window hints are already the default
+    (GLFW/glfwDefaultWindowHints)
+    ;; the window will stay hidden after creation
+    (GLFW/glfwWindowHint GLFW/GLFW_VISIBLE GLFW/GLFW_FALSE)
+    ;; the window will be resizable
+    (GLFW/glfwWindowHint GLFW/GLFW_RESIZABLE GLFW/GLFW_TRUE)
 
-  ;;;; configure GLFW  
-  ;; optional, the current window hints are already the default
-  (GLFW/glfwDefaultWindowHints)
-  ;; the window will stay hidden after creation
-  (GLFW/glfwWindowHint GLFW/GLFW_VISIBLE GLFW/GLFW_FALSE)
-  ;; the window will be resizable
-  (GLFW/glfwWindowHint GLFW/GLFW_RESIZABLE GLFW/GLFW_TRUE)
+    ;; create the window and store it in the state
+    (let [window (GLFW/glfwCreateWindow initial-window-width initial-window-height "Hello, World!" 0 0)]
+      (when (zero? window)
+        (throw (IllegalStateException. "Unable to create the GLFW window")))
+      (let [state (assoc state :window window)]
 
-  ;; create the window and swap! it into the global state atom
-  (let [window (GLFW/glfwCreateWindow initial-window-width initial-window-height "Hello, World!" 0 0)]
-    (when (zero? window)
-      (throw (IllegalStateException. "Unable to create the GLFW window")))
-    (swap! state assoc :window window)
+        ;; @NOTE can stabilise framerate with this when framerate is uncapped
+        ;; Hide mouse cursor, for some reason in X11 moving the mouse tanks framerate
+        ;; (GLFW/glfwSetInputMode window GLFW/GLFW_CURSOR GLFW/GLFW_CURSOR_DISABLED)
 
-    ;; @NOTE can stabilise framerate with this when framerate is uncapped
-    ;; Hide mouse cursor, for some reason in X11 moving the mouse tanks framerate
-    ;; (GLFW/glfwSetInputMode window GLFW/GLFW_CURSOR GLFW/GLFW_CURSOR_DISABLED)
+        ;; set up a key callback, it will be called every time a key is
+        ;; pressed, repeated or released
+        (GLFW/glfwSetKeyCallback
+         window
+         (reify GLFWKeyCallbackI
+           (invoke [this win k scancode action mods]
+             ;; quit on ESC
+             (when (and (= k GLFW/GLFW_KEY_ESCAPE)
+                        (= action GLFW/GLFW_RELEASE))
+               ;; we will detect this in the window loop
+               (GLFW/glfwSetWindowShouldClose window true)))))
 
-    ;; set up a key callback, it will be called every time a key is
-    ;; pressed, repeated or released
-    (GLFW/glfwSetKeyCallback
-     window
-     (reify GLFWKeyCallbackI
-       (invoke [this win k scancode action mods]
-         ;; quit on ESC
-         (when (and (= k GLFW/GLFW_KEY_ESCAPE)
-                    (= action GLFW/GLFW_RELEASE))
-           ;; we will detect this in the window loop
-           (GLFW/glfwSetWindowShouldClose window true)))))
+        ;; @TODO: need a way of handling user inputs in a functional(-ish) way!
+        ;; ;; when the mouse moves in the window set the position of the
+        ;; ;; example sprite to the cursor
+        ;; (GLFW/glfwSetCursorPosCallback
+        ;;  window
+        ;;  (reify GLFWCursorPosCallbackI
+        ;;    (invoke [this win xpos ypos]
+        ;;      (swap! state (fn [st]
+        ;;                     (sprite/update-sprites
+        ;;                      st
+        ;;                      (sprite/has-group :example)
+        ;;                      #(assoc % :pos [xpos ypos])))))))
 
-    ;; when the mouse moves in the window set the position of the
-    ;; example sprite to the cursor
-    (GLFW/glfwSetCursorPosCallback
-     window
-     (reify GLFWCursorPosCallbackI
-       (invoke [this win xpos ypos]
-         (swap! state (fn [st]
-                        (sprite/update-sprites
-                         st
-                         (sprite/has-group :example)
-                         #(assoc % :pos [xpos ypos])))))))
+        ;; when we press/release a mouse button print it
+        (GLFW/glfwSetMouseButtonCallback
+         window
+         (reify GLFWMouseButtonCallbackI
+           (invoke [this win button action mods]
+             (let [buttons [:left :right :middle]
+                   actions [:released :pressed]]
+               (prn (get buttons button) (get actions action))))))
 
-    ;; when we press/release a mouse button print it
-    (GLFW/glfwSetMouseButtonCallback
-     window
-     (reify GLFWMouseButtonCallbackI
-       (invoke [this win button action mods]
-         (let [buttons [:left :right :middle]
-               actions [:released :pressed]]
-           (prn (get buttons button) (get actions action))))))
+        (let [[window-w window-h] (u/window-size window)
+              ;; get the resolution of the primary monitor
+              vidmode (-> (GLFW/glfwGetPrimaryMonitor)
+                          (GLFW/glfwGetVideoMode))
+              x-pos (/ (- (.width vidmode)
+                          window-w)
+                       2)
+              y-pos (/ (- (.height vidmode)
+                          window-h)
+                       2)]
+          ;; centre the window
+          (GLFW/glfwSetWindowPos window x-pos y-pos))
 
-    (let [[window-w window-h] (u/window-size window)
-          ;; get the resolution of the primary monitor
-          vidmode (-> (GLFW/glfwGetPrimaryMonitor)
-                      (GLFW/glfwGetVideoMode))
-          x-pos (/ (- (.width vidmode)
-                      window-w)
-                   2)
-          y-pos (/ (- (.height vidmode)
-                      window-h)
-                   2)]
-      ;; centre the window
-      (GLFW/glfwSetWindowPos window x-pos y-pos))
+        ;; make the OpenGL context current
+        (GLFW/glfwMakeContextCurrent window)
+        ;; enable v-sync (set this to 0 to uncap framerate and run fast as possible)
+        (GLFW/glfwSwapInterval 1)
+        ;; make the window visible
+        (GLFW/glfwShowWindow window)
 
-    ;; make the OpenGL context current
-    (GLFW/glfwMakeContextCurrent window)
-    ;; enable v-sync (set this to 0 to uncap framerate and run fast as possible)
-    (GLFW/glfwSwapInterval 1)
-    ;; make the window visible
-    (GLFW/glfwShowWindow window)
+        ;; this line is critical for LWJGL's interoperation with GLFW's
+        ;; OpenGL context, or any context that is managed externally. LWJGL
+        ;; detects the context that is current in the current thread,
+        ;; creates the GLCapabilities instance and makes the OpenGL bindings
+        ;; available for use
+        (GL/createCapabilities)
 
-    ;; this line is critical for LWJGL's interoperation with GLFW's
-    ;; OpenGL context, or any context that is managed externally. LWJGL
-    ;; detects the context that is current in the current thread,
-    ;; creates the GLCapabilities instance and makes the OpenGL bindings
-    ;; available for use
-    (GL/createCapabilities)
+        ;; @TODO: this should use the current screen size, not the initial (we're crashing when window is resized right now)
+        ;; @NOTE test setting orthographic mode so we can use pixel positions for vertices
+        (reset-ortho-projection initial-window-width initial-window-height)
+        ;; set resize callback
+        (GLFW/glfwSetFramebufferSizeCallback
+         window
+         (reify GLFWFramebufferSizeCallbackI
+           (invoke [this win w h]
+             ;; update the GL viewport to cover the new window
+             (GL11/glViewport 0 0 w h)
+             ;; re-build the ortho projection matrix to match the new size
+             (reset-ortho-projection w h))))
 
-    ;; @TODO: this should use the current screen size, not the initial (we're crashing when window is resized right now)
-    ;; @NOTE test setting orthographic mode so we can use pixel positions for vertices
-    (reset-ortho-projection initial-window-width initial-window-height)
-    ;; set resize callback
-    (GLFW/glfwSetFramebufferSizeCallback
-     window
-     (reify GLFWFramebufferSizeCallbackI
-       (invoke [this win w h]
-         ;; update the GL viewport to cover the new window
-         (GL11/glViewport 0 0 w h)
-         ;; re-build the ortho projection matrix to match the new size
-         (reset-ortho-projection w h))))
+        ;;;; initialise text rendering stuff
+        ;; create NanoVG context
+        (let [vg (NanoVGGL3/nvgCreate (bit-or NanoVGGL3/NVG_ANTIALIAS
+                                              NanoVGGL3/NVG_STENCIL_STROKES))
+              ;; load a font
+              font (NanoVG/nvgCreateFont vg "sans" "resources/font/UbuntuMono-Regular.ttf")
+              ;; stick them in the state
+              state (-> state
+                        (assoc :v vg)
+                        (assoc :font font))]
 
-    ;;;; initialise text rendering stuff
-    ;; create NanoVG context
-    (let [vg (NanoVGGL3/nvgCreate (bit-or NanoVGGL3/NVG_ANTIALIAS
-                                          NanoVGGL3/NVG_STENCIL_STROKES))
-          ;; load a font
-          font (NanoVG/nvgCreateFont vg "sans" "resources/font/UbuntuMono-Regular.ttf")]
-      ;; stick them in the state
-      (swap! state #(assoc % :vg vg :font font)))
+          ;; enable transprency for drawing images
+          (GL11/glEnable GL11/GL_BLEND)
+          (GL30/glBlendFuncSeparate GL11/GL_SRC_ALPHA GL11/GL_ONE_MINUS_SRC_ALPHA GL11/GL_ONE GL11/GL_ONE_MINUS_SRC_ALPHA)
 
-    ;; enable transprency for drawing images
-    (GL11/glEnable GL11/GL_BLEND)
-    (GL30/glBlendFuncSeparate GL11/GL_SRC_ALPHA GL11/GL_ONE_MINUS_SRC_ALPHA GL11/GL_ONE GL11/GL_ONE_MINUS_SRC_ALPHA)
+          ;; optionally start audio
+          (let [audio (when audio? (init-audio))
+                state (assoc state :audio audio)]
 
-    ;; start audio
-    (when audio?
-      (let [audio (init-audio)]
-        (swap! state #(assoc % :audio audio))))
+            ;; start polling for events
+            (start-event-polling state)
 
-    ;; start polling for events
-    (start-event-polling)))
+            ;; return the initial game sate
+            state))))))
 
 (defn draw-background!
   [[r g b a]]
@@ -270,22 +273,22 @@
   ;; clear the frameBuffer
   (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT)))
 
-;; @TODO: this should be a text-sprite
-(defn draw-text!
-  [vg font]
-  (let [old-state (capture-gl-state)]
-    (let [[window-w window-h] (u/window-size (:window @state))]
-      (NanoVG/nvgBeginFrame vg window-w window-h 1)
-      (NanoVG/nvgFontSize vg 64)
-      (NanoVG/nvgFontFace vg "sans")
-      (with-open [stack (MemoryStack/stackPush)]
-        (let [white (NVGColor/malloc stack)
-              f0 (float 0)
-              f1 (float 1)]
-          (NanoVG/nvgFillColor vg (NanoVG/nvgRGBAf f1 f1 f1 f1 white))))
-      (NanoVG/nvgText vg (float 50) (float 175) "Hello NanoVG!" )
-      (NanoVG/nvgEndFrame vg))
-    (restore-gl-state old-state)))
+;; ;; @TODO: this should be a text-sprite, the `:vg` from state should be closured
+;; (defn draw-text!
+;;   [state vg font]
+;;   (let [old-state (capture-gl-state)]
+;;     (let [[window-w window-h] (u/window-size (:window state))]
+;;       (NanoVG/nvgBeginFrame vg window-w window-h 1)
+;;       (NanoVG/nvgFontSize vg 64)
+;;       (NanoVG/nvgFontFace vg "sans")
+;;       (with-open [stack (MemoryStack/stackPush)]
+;;         (let [white (NVGColor/malloc stack)
+;;               f0 (float 0)
+;;               f1 (float 1)]
+;;           (NanoVG/nvgFillColor vg (NanoVG/nvgRGBAf f1 f1 f1 f1 white))))
+;;       (NanoVG/nvgText vg (float 50) (float 175) "Hello NanoVG!" )
+;;       (NanoVG/nvgEndFrame vg))
+;;     (restore-gl-state old-state)))
 
 (defn update-game
   "Update the game state based on the current scenes `:update-fn`."
@@ -335,54 +338,54 @@
    :on-close-fn default-on-close})
 
 (defn game
+  "Create a game config map"
   [{:keys [init-scenes-fn current-scene]
     :or {init-scenes-fn (constantly {})
          current-scene :none}
     :as override-opts}]
   (merge default-opts override-opts))
 
-;;@TODO: can we make this more functional? maybe not?
 (defn main-loop
-  []
-  ;; run the rendering loop until the user has attempted to close the
-  ;; window or has pressed the ESC key.
-  (while (not (GLFW/glfwWindowShouldClose (:window @state)))
-    ;; update the state
-    (swap! state update-game)
+  [st]
+  ;; update the state
+  (let [new-st (update-game st)]
 
     ;; draw everything
-    (draw-game! @state)))
+    (draw-game! new-st)
+
+    ;; return the new state
+    new-st))
 
 (defn start!
   [{:keys [init-scenes-fn current-scene] :as game}]
-  (println "Hello LWJGL! Version:" (Version/getVersion))
+  (let [initialised-lwjgl (init game)
+        window (:window initialised-lwjgl)
+        audio (:audio initialised-lwjgl)
+        scenes (init-scenes-fn initialised-lwjgl)
+        state (merge initialised-lwjgl {:scenes scenes})]
+    (try
+      (doall
+       (iteration
+        main-loop
+        :initk state
+        ;; run the rendering loop until the user has attempted to
+        ;; close the window or has pressed the ESC key.
+        :somef (fn [st]
+                 (not (GLFW/glfwWindowShouldClose (:window st))))))
+      (catch Exception e
+        (prn e)))
 
-  (init game)
+    ;; @TODO: call game defined `:on-close-fn`
 
-  (swap! state merge game {:scenes (init-scenes-fn @state)})
+    ;; clean up audio stuff on close
+    (when audio?
+      (cleanup-audio audio))
 
-  (try
-    (main-loop)
-    (catch Exception e
-      (prn e)))
+    ;; free window callbacks and destroy the window
+    (Callbacks/glfwFreeCallbacks window)
+    (GLFW/glfwDestroyWindow window)
 
-  ;; @TODO: call game defined `:on-close-fn`
-
-  ;; clean up audio stuff on close
-  (when audio?
-    (cleanup-audio @state))
-
-  ;; free window callbacks and destroy the window
-  (Callbacks/glfwFreeCallbacks (:window @state))
-  (GLFW/glfwDestroyWindow (:window @state))
-
-  ;; terminate GLFW and terminate the error callback
-  (GLFW/glfwTerminate)
-  (-> (GLFW/glfwSetErrorCallback nil)
-      .free))
-
-(comment
-
-  (clunk.example-game/main)
-
-  )
+    ;; terminate GLFW and terminate the error callback
+    (GLFW/glfwTerminate)
+    (-> (GLFW/glfwSetErrorCallback nil)
+        .free)))
