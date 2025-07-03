3ad0bcbf5b4e485c15f2c348b918f0b396970a75
Kimbsy <lordkimber@gmail.com>
2025-07-03 21:22:29 +0100

    Using deployed version in example games
    Adding utility script to update clunk version in examples

--- examples/tweens/project.clj
+++ examples/tweens/project.clj
@@ -1,6 +1,6 @@
 (defproject tweens "0.1.0"
   :dependencies [[org.clojure/clojure "1.12.1"]
-                 [com.kimbsy/clunk "0.1.0-SNAPSHOT"]]
+                 [com.kimbsy/clunk "0.1.0"]]
   :main ^:skip-aot tweens.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
