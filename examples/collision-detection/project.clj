b799e70a6a72cf53681a0d951605291f5105860f
Kimbsy <lordkimber@gmail.com>
2025-07-16 11:34:06 +0100

    update example versions

--- examples/collision-detection/project.clj
+++ examples/collision-detection/project.clj
@@ -1,6 +1,6 @@
 (defproject collision-detection "0.1.0"
   :dependencies [[org.clojure/clojure "1.12.1"]
-                 [com.kimbsy/clunk "0.4.0-SNAPSHOT"]]
+                 [com.kimbsy/clunk "0.4.2"]]
   :main ^:skip-aot collision-detection.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
