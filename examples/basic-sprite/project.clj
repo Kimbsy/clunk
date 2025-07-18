6cc47a36528d6f60edbf2d5ea7621d71422b250e
Kimbsy <lordkimber@gmail.com>
2025-07-18 21:19:09 +0100

    Version 0.6.0

--- examples/basic-sprite/project.clj
+++ examples/basic-sprite/project.clj
@@ -1,6 +1,6 @@
 (defproject basic-sprite "0.1.0"
   :dependencies [[org.clojure/clojure "1.12.1"]
-                 [com.kimbsy/clunk "0.5.0"]]
+                 [com.kimbsy/clunk "0.6.0"]]
   :main ^:skip-aot basic-sprite.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
