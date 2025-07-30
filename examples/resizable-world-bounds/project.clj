3f08b8f55162e7e81fd97647497be1151b981d11
Kimbsy <lordkimber@gmail.com>
2025-07-30 20:47:39 +0100

    Version 1.0.0 :tada:

--- examples/resizable-world-bounds/project.clj
+++ examples/resizable-world-bounds/project.clj
@@ -1,6 +1,6 @@
 (defproject resizable-world-bounds "0.1.0"
   :dependencies [[org.clojure/clojure "1.12.1"]
-                 [com.kimbsy/clunk "0.10.0"]]
+                 [com.kimbsy/clunk "1.0.0"]]
   :main ^:skip-aot resizable-world-bounds.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.kimbsy/clunk "1.0.0-SNAPSHOT"
+(defproject com.kimbsy/clunk "1.0.0"
   :description "A 2D game engine based on LWJGL (Light Weight Java Game Library)"
   :url "https://github.com/Kimbsy/clunk"
   :license {:name "MIT License"
