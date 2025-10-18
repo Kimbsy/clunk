37d02b4136e3e6e68a564ad34edb2d9484781550
Kimbsy <lordkimber@gmail.com>
2025-10-18 20:37:13 +0100

    WORKING using core profile and shaders to render polygons

--- minimal/src/minimal/core.clj
+++ minimal/src/minimal/core.clj
@@ -1,5 +1,6 @@
 (ns minimal.core
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clojure.math :as math])
   (:import (org.lwjgl BufferUtils)
            (org.lwjgl.glfw Callbacks
                            GLFW
@@ -14,61 +15,11 @@
            (org.lwjgl.opengl GL
                              GL11
                              GL14
-                             GL30)
+                             GL15
+                             GL20
+                             GL30
+                             GL40)
            (org.lwjgl.system MemoryStack)))
-
-(defn capture-gl-state
-  "Capture the current blending state of GL so we can draw NanoVG
-  text (which blats the config) before restoring the original state
-  for drawing images/shapes."
-  []
-  {:src-rgb (GL11/glGetInteger GL30/GL_BLEND_SRC_RGB)
-   :src-alpha (GL11/glGetInteger GL30/GL_BLEND_SRC_ALPHA)
-   :dst-rgb (GL11/glGetInteger GL14/GL_BLEND_DST_RGB)
-   :dst-alpha (GL11/glGetInteger GL11/GL_DST_ALPHA)
-   :blend-enabled? (GL11/glIsEnabled GL11/GL_BLEND)})
-
-(defn restore-gl-state
-  "Restore the original blending state of GL for drawing images/shapes."
-  [{:keys [src-rgb src-alpha dst-rgb dst-alpha blend-enabled?]}]
-  (if blend-enabled?
-    (GL11/glEnable GL11/GL_BLEND)
-    (GL11/glDisable GL11/GL_BLEND))
-  (GL30/glBlendFuncSeparate src-rgb dst-rgb src-alpha dst-alpha))
-
-(defn draw-text!
-  [vg content font vg-color]
-  (let [[r g b a] (map float [1 1 1 1])
-        [window-w window-h] [800 600]
-        old-state (capture-gl-state)]
-    (NanoVG/nvgBeginFrame vg window-w window-h 1)
-    (NanoVG/nvgFontSize vg 32)
-    (NanoVG/nvgFontFace vg font)
-    (NanoVG/nvgFillColor vg (NanoVG/nvgRGBAf r g b a vg-color))
-
-    (NanoVG/nvgSave vg)
-    (NanoVG/nvgTranslate vg (float 100) (float 300))
-    (NanoVG/nvgText vg (float 0) (float 0) content)
-    (NanoVG/nvgRestore vg)
-
-    (NanoVG/nvgEndFrame vg)
-    (restore-gl-state old-state)))
-
-(defn create-font
-  "Load a .ttf from a classpath resource and register it as `name` in
-  the NanoVG context `vg`."
-  [vg name path]
-  (prn path)
-  (prn (io/resource path))
-  (with-open [is (io/input-stream (io/resource path))]
-    ;; read all bytes
-    (let [ba (byte-array (.available is))]
-      (.read is ba)
-      ;; wrap into a direct ByteBuffer
-      (let [buf (doto (BufferUtils/createByteBuffer (alength ba))
-                  (.put ba)
-                  (.flip))]
-        (NanoVG/nvgCreateFontMem vg name buf false)))))
 
 (defn -main
   []
@@ -78,23 +29,12 @@
   (when (not (GLFW/glfwInit))
     (throw (IllegalStateException. "Unable to initialise GLFW")))
 
-  ;; optional, the current window hints are already the default
-  (GLFW/glfwDefaultWindowHints)
-
-
-  ;; @TODO: testing mac requirements for nanovg
   ;; request OpenGL 3.2 core profile
   (GLFW/glfwWindowHint GLFW/GLFW_CONTEXT_VERSION_MAJOR 3)
-  (GLFW/glfwWindowHint GLFW/GLFW_CONTEXT_VERSION_MINOR 2)
+  (GLFW/glfwWindowHint GLFW/GLFW_CONTEXT_VERSION_MINOR 3)
   (GLFW/glfwWindowHint GLFW/GLFW_OPENGL_PROFILE GLFW/GLFW_OPENGL_CORE_PROFILE)
   ;; required on macOS
   (GLFW/glfwWindowHint GLFW/GLFW_OPENGL_FORWARD_COMPAT GLFW/GLFW_TRUE)
-
-
-  ;; the window will stay hidden after creation
-  (GLFW/glfwWindowHint GLFW/GLFW_VISIBLE GLFW/GLFW_FALSE)
-  ;; the window will be resizable
-  (GLFW/glfwWindowHint GLFW/GLFW_RESIZABLE GLFW/GLFW_TRUE)
 
   (let [window (GLFW/glfwCreateWindow 800 600 "Hello, World!" 0 0)]
     (when (zero? window)
@@ -114,45 +54,122 @@
     ;; available for use
     (GL/createCapabilities)
 
-    ;; ;; reset orthographic projection
-    ;; (GL11/glMatrixMode GL11/GL_PROJECTION)
-    ;; (GL11/glLoadIdentity)
-    ;; ;; left, right, top, bottom in pixel units
-    ;; (GL11/glOrtho 0 800 600 0 -1 1)
-    ;; ;; go back to model view
-    ;; (GL11/glMatrixMode GL11/GL_MODELVIEW)
-    ;; (GL11/glLoadIdentity)
+    ;; setting up primitive drawing
+    (let [vertex-size 3 ;; x,y,z (even though z will always be 0)
+          ;; a Vertex Buffer Object (VBO) for holding the vertex data
+          vbo (GL15/glGenBuffers)
+          ;; a Vertex Array Object (VAO) for holding the attributes for the vbo
+          vao (GL30/glGenVertexArrays)
+          ;; an Element Array Buffer (EBO) for storing the indices of our vertices
+          ebo (GL15/glGenBuffers)
 
-    ;; @TODO: testing text
-    ;; create NanoVG context
-    (let [vg (NanoVGGL3/nvgCreate (bit-or NanoVGGL3/NVG_ANTIALIAS
-                                          NanoVGGL3/NVG_STENCIL_STROKES))
-          ;; load a font
-          font-name "UbuntuMono-Regular"
-          font (create-font vg font-name "font/UbuntuMono-Regular.ttf")
-          vg-color (NVGColor/create)]
+          ;; define a rectangle using 4 vertices and 2 triangles
+          vertices (float-array [ 0.5  0.5 0   ;; top right
+                                  0.5 -0.5 0   ;; bottom right
+                                 -0.5 -0.5 0   ;; bottom left
+                                 -0.5  0.5 0]) ;; top left
+          indices (int-array [0 1 3    ;; first tri
+                              1 2 3])] ;; second tri
 
-      ;; enable transparency
-      (GL11/glEnable GL11/GL_BLEND)
-      (GL30/glBlendFuncSeparate GL11/GL_SRC_ALPHA GL11/GL_ONE_MINUS_SRC_ALPHA GL11/GL_ONE GL11/GL_ONE_MINUS_SRC_ALPHA)
-      
-      ;; loop
-      (while (not (GLFW/glfwWindowShouldClose window))
+      ;; bind the vao, now everything following should be inside it
+      (GL30/glBindVertexArray vao)
 
-        ;; poll for events
-        (GLFW/glfwPollEvents)
+      ;; copy the vertex data into the vbo
+      (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER vbo)
+      (GL15/glBufferData GL15/GL_ARRAY_BUFFER vertices GL15/GL_STATIC_DRAW)
 
-        ;; draw background
-        ;; set the clear colour
-        (GL11/glClearColor 1.0 0.6 0.5 1)
-        ;; clear the frameBuffer
-        (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
+      ;; put the index aray in an element buffer for opengl to use
+      (GL15/glBindBuffer GL15/GL_ELEMENT_ARRAY_BUFFER ebo)
+      (GL15/glBufferData GL15/GL_ELEMENT_ARRAY_BUFFER indices GL15/GL_STATIC_DRAW)
 
-        ;; @TODO: testing draw text
-        (draw-text! vg "Drawing text seems to work" font-name vg-color)
+      ;; set vertex attribute pointers
+      (GL30/glVertexAttribPointer 0 ;; attribute at location 0 in the shader
+                                  vertex-size
+                                  GL15/GL_FLOAT
+                                  false
+                                  (* vertex-size (Float/BYTES))
+                                  0)
+      ;; enable the vertex attribute
+      (GL30/glEnableVertexAttribArray 0) ;; location 0
 
-        ;; swap buffers
-        (GLFW/glfwSwapBuffers window)))
+      ;; set up a shader program (a vertex shader and a fragment shader)
+      (let [vert-shader-source (slurp (io/resource "shader/basic.vert.glsl"))
+            frag-shader-source (slurp (io/resource "shader/basic.frag.glsl"))
+            vert-shader (GL20/glCreateShader GL20/GL_VERTEX_SHADER)
+            frag-shader (GL20/glCreateShader GL20/GL_FRAGMENT_SHADER)]
+
+        ;; set the source for the vertex shader and compile it
+        (GL20/glShaderSource vert-shader vert-shader-source)
+        (GL20/glCompileShader vert-shader)
+
+        ;; set the source for the fragment shader and compile it
+        (GL20/glShaderSource frag-shader frag-shader-source)
+        (GL20/glCompileShader frag-shader)
+
+
+        ;; @TODO: check that the shader compilation has worked?
+
+
+        
+
+        ;; create a shader program
+        (let [shader-program (GL20/glCreateProgram)]
+
+          ;; attach the shaders and then link the program
+          (GL20/glAttachShader shader-program vert-shader)
+          (GL20/glAttachShader shader-program frag-shader)
+          (GL20/glLinkProgram shader-program)
+
+          ;; we don't need the shaders anymore, they're linked in the program now
+          (GL20/glDeleteShader vert-shader)
+          (GL20/glDeleteShader frag-shader)
+
+          ;; LOOP ;;
+          (while (not (GLFW/glfwWindowShouldClose window))
+
+            ;; poll for events
+            (GLFW/glfwPollEvents)
+
+            ;; draw background
+            ;; set the clear colour
+            (GL11/glClearColor 1.0 0.6 0.5 1)
+            ;; clear the frameBuffer
+            (GL11/glClear GL11/GL_COLOR_BUFFER_BIT)
+
+
+
+            ;; draw the shape ;;
+
+            ;; everything after this will use our shaders
+            (GL20/glUseProgram shader-program)
+
+            ;; set the uniform for the shape colour
+            (let [time-value (GLFW/glfwGetTime)
+                  g (+ 0.5 (/ (math/sin time-value)
+                              2))
+                  ;; grab the locatoin of the uniform attribute from the linked shader program
+                  vertex-color-location (GL20/glGetUniformLocation shader-program "color")]
+              ;; set the uniform value
+              (GL20/glUniform4f vertex-color-location 0 g 0 1))
+
+            ;; bind our VAO
+            (GL30/glBindVertexArray vao)
+
+            ;; uncomment to draw wireframe rather than fill
+            ;; (GL11/glPolygonMode GL11/GL_FRONT_AND_BACK GL11/GL_LINE)
+
+            ;; draw the triangles
+            (GL40/glDrawElements GL40/GL_TRIANGLES 6 GL11/GL_UNSIGNED_INT 0)
+
+            ;; unbind the VAO
+            (GL30/glBindVertexArray 0)
+
+
+
+            
+
+            ;; swap buffers to draw everything
+            (GLFW/glfwSwapBuffers window)))))
 
     ;; free window callbacks and destroy the window
     (Callbacks/glfwFreeCallbacks window)
