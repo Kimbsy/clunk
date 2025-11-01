8c1284dc5141707f5fa980a192ab8eba13b31c2f
Kimbsy <lordkimber@gmail.com>
2025-11-01 21:41:49 +0000

    Preserve alpha component of colours in palette utils

--- src/clunk/palette.clj
+++ src/clunk/palette.clj
@@ -23,7 +23,7 @@
   "Darken a colour by 30%, preserving alpha component if present."
   [[r g b a :as color]]
   (if a
-    [(max 0 (* r 0.7)) (max 0 (* g 0.7)) (max 0 (* b 0.7)) (max 0 (* a 0.7))]
+    [(max 0 (* r 0.7)) (max 0 (* g 0.7)) (max 0 (* b 0.7)) a]
     [(max 0 (* r 0.7)) (max 0 (* g 0.7)) (max 0 (* b 0.7))]))
 
 (defn lighten
@@ -32,7 +32,7 @@
   ;; (/ 1 0.7) => 1.4285714285714286
   (let [factor 1.4285714285714286]
     (if a
-      [(min 1 (* r factor)) (min 1 (* g factor)) (min 1 (* b factor)) (min 1 (* a factor))]
+      [(min 1 (* r factor)) (min 1 (* g factor)) (min 1 (* b factor)) a]
       [(min 1 (* r factor)) (min 1 (* g factor)) (min 1 (* b factor))])))
 
 (def black [0 0 0 1])
