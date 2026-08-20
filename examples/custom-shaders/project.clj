3447b5be4b30289527dea5e96f11fb5f08e63502
Kimbsy <lordkimber@gmail.com>
2026-08-20 16:50:18 +0100

    Version 2.0.2

--- examples/custom-shaders/project.clj
+++ examples/custom-shaders/project.clj
@@ -1,6 +1,6 @@
 (defproject custom-shaders "0.1.0"
   :dependencies [[org.clojure/clojure "1.12.1"]
-                 [com.kimbsy/clunk "2.0.2-SNAPSHOT"]]
+                 [com.kimbsy/clunk "2.0.2"]]
   :main ^:skip-aot custom-shaders.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
